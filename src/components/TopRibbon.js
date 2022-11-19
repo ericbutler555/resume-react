7509864d945b7398c2941c045fad94e49fd9fe25
Eric Butler <ericbutler555@gmail.com>
2022-11-18 22:48:26 -0500

    Update TopRibbon text

--- src/components/TopRibbon.js
+++ src/components/TopRibbon.js
@@ -4,7 +4,11 @@ function TopRibbon() {
   return (
     <div className="top-ribbon">
       <p>
-        This is the React JS version of this page. I also made an Angular version. <a href="https://ericbutler555.github.io/resume-ng/">Switch to Angular</a>
+        This is the React JS version of this page.{" "}
+        <a href="https://github.com/ericbutler555/resume-react/" target="_blank">
+          Here's the repo
+        </a>
+        . I also made an <a href="https://ericbutler555.github.io/resume-ng/">Angular version</a>.
       </p>
     </div>
   );
