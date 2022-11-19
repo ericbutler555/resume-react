e54c379abcdfdd59c0a1d1c4ceea1fcf1ebb4686
Eric Butler <ericbutler555@gmail.com>
2022-11-18 21:09:10 -0500

    Build out Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,42 @@
+import "../scss/Skills.scss";
+
 function Skills() {
-  return <div className="skills">Skills go here.</div>;
+  // prettier-ignore
+  const skills = [
+    "Semantic HTML",
+    "CSS, Sass & Less",
+    "JavaScript & ES6",
+    "Angular",
+    "Next.js",
+    "jQuery",
+    "REST APIs",
+    "Responsive web design",
+    "WordPress & custom themes",
+    "PHP",
+    "SQL",
+    "Kentico CMS",
+    "Adobe Experience Manager",
+    "Gulp.js",
+    "Git & GitHub",
+    "TFS",
+    "HTML email",
+    "Greensock (GSAP)",
+    "Landing pages",
+    "Google Tag Manager & Analytics",
+    "Adobe InDesign, Photoshop, Illustrator & XD",
+    "Figma"
+  ];
+
+  return (
+    <section id="skills" className="skills">
+      <h2>Skills &amp; Technologies</h2>
+      <ul className="skills-items">
+        {skills.map((skill, i) => (
+          <li key={i}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default Skills;
