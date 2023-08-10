b45c314d02f65b5f725ad4ee1c0f4aad8118d791
Eric Butler <ericbutler555@gmail.com>
2023-08-10 12:37:30 -0400

    Tweak top ribbon text

--- src/components/TopRibbon.js
+++ src/components/TopRibbon.js
@@ -4,7 +4,7 @@ function TopRibbon() {
   return (
     <div className="top-ribbon">
       <p>
-        This is the React JS version of this page. <a href="https://github.com/ericbutler555/resume-react/">Here's the repo</a>. I also made an <a href="https://ericbutler555.github.io/resume-ng/">Angular version</a>.
+        This page was built in React. <a href="https://github.com/ericbutler555/resume-react/">Here's the repo</a>. I also made an <a href="https://ericbutler555.github.io/resume-ng/">Angular version</a>.
       </p>
     </div>
   );
