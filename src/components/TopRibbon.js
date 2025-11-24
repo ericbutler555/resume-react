2cc63632975f9475a242b1febdb65a3c73eff4ec
Eric Butler <ericbutler555@gmail.com>
2025-11-23 22:15:14 -0500

    Change URL of Angular site in TopRibbon

--- src/components/TopRibbon.js
+++ src/components/TopRibbon.js
@@ -4,7 +4,7 @@ function TopRibbon() {
   return (
     <div className="top-ribbon">
       <p>
-        This page was built in React. <a href="https://github.com/ericbutler555/resume-react/">Here's the repo</a>. I also made an <a href="https://ericbutler555.github.io/resume-ng/">Angular version</a>.
+        This page was built in React. <a href="https://github.com/ericbutler555/resume-react/">Here's the repo</a>. I also made an <a href="https://ng.ericbutler555.com/">Angular version</a>.
       </p>
     </div>
   );
